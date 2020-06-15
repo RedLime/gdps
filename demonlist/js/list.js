0f366a35a440c47ddea659879fd3eb805dc5bd09
RedLime <redlimerl@gmail.com>
2020-06-15 14:01:49 +0900

    데몬리스트 업데이트

---
+++ demonlist/js/list.js
@@ -0,0 +1,300 @@
+const d = {
+    "list": [
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "Delta outerface",
+            author: "YoungUng2",
+            mapInfo: {
+                simpleExp: "Super Ultra Hyper Epic And Nong :(",
+                uploadDate: "2020.05.01",
+                verifingAtt: "",
+                id: "1323 / 696969",
+                long: "2분 1초",
+                highest: "1",
+                more: "",
+            },
+			percentToQualify: 44,
+            verificationVid: "https://www.youtube.com/watch?v=12jEX10STuA",
+            key: 0
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "Incurable",
+            author: "Blancookie",
+            mapInfo: {
+                simpleExp: "Renevant : made in china",
+                uploadDate: "2020.05.13",
+                verifingAtt: "",
+                id: "2444 / Copy 불가",
+                long: "1분 48초",
+                highest: "2",
+                more: "",
+            },
+			percentToQualify: 50,
+            verificationVid: "https://www.youtube.com/watch?v=1nmFQGMf0f4",
+            key: 1
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "Sandstorm",
+            author: "YoungUng2",
+            mapInfo: {
+                simpleExp: "Hyper Epic",
+                uploadDate: "2020.04.25",
+                verifingAtt: "",
+                id: "540 / 858585",
+                long: "1분 1초",
+                highest: "3",
+                more: "",
+            },
+			percentToQualify: 50,
+            verificationVid: "https://www.youtube.com/watch?v=puEA266G04A",
+            key: 2
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "Duplication",
+            author: "Blancookie",
+            mapInfo: {
+                simpleExp: "Cognition: made in China 2490Attempts Update: bug fixed and buffed",
+                uploadDate: "2020.04.25",
+                verifingAtt: "",
+                id: "395 / 051108",
+                long: "1분 2초",
+                highest: "4",
+                more: "",
+            },
+			percentToQualify: 50,
+            verificationVid: "https://www.youtube.com/watch?v=v9zI0kJNOTY",
+            key: 3
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "black",
+            author: "Esencia [TheRealMap]",
+            mapInfo: {
+                simpleExp: "my last color series level! next series level named \"valley\" enjoy! :)",
+                uploadDate: "2020.05.02",
+                verifingAtt: "",
+                id: "1416 / 101010",
+                long: "1분 2초",
+                highest: "5",
+                more: "",
+            },
+			percentToQualify: 50,
+            verificationVid: "",//베파 영상 없음
+            key: 4
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "nautilus",
+            author: "Revitin",
+            mapInfo: {
+                simpleExp: "o<-<",
+                uploadDate: "2020.04.26",
+                verifingAtt: "",
+                id: "628 / Free Copy",
+                long: "31초",
+                highest: "6",
+                more: "",
+            },
+			percentToQualify: 60,
+            verificationVid: "",//베파 영상 없음
+            key: 5
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "sunset",
+            author: "YoungUng2",
+            mapInfo: {
+                simpleExp: "Ultra Epic Update Update Nerf Update",
+                uploadDate: "2020.04.24",
+                verifingAtt: "",
+                id: "259 / 585858",
+                long: "1분 1초",
+                highest: "7",
+                more: "",
+            },
+			percentToQualify: 42,
+            verificationVid: "https://www.youtube.com/watch?v=G4_q-OUDp1E",
+            key: 6
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "After Madness",
+            author: "Lebi",
+            mapInfo: {
+                simpleExp: "Stereo Madness 'After'",
+                uploadDate: "2020.05.11",
+                verifingAtt: "",
+                id: "2249 / Copy 불가",
+                long: "2분 05초",
+                highest: "8",
+                more: "",
+            },
+			percentToQualify: 45,
+            verificationVid: "https://www.youtube.com/watch?v=WBfsPa8nBo8",
+            key: 7
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "ZwE1",
+            author: "estachi",
+            mapInfo: {
+                simpleExp: "Dual is love",
+                uploadDate: "2020.04.24",
+                verifingAtt: "",
+                id: "203 / 050320",
+                long: "1분 03초",
+                highest: "9",
+                more: "",
+            },
+			percentToQualify: 48,
+            verificationVid: "",//베파 영상없음
+            key: 8
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "wrong",
+            author: "ari1479",
+            mapInfo: {
+                simpleExp: "bok boot (made,verified on mobile)",
+                uploadDate: "2020.05.03",
+                verifingAtt: "",
+                id: "1504 / 001479",
+                long: "52초",
+                highest: "10",
+                more: "",
+            },
+			percentToQualify: 48,
+            verificationVid: "https://www.youtube.com/watch?v=49w1IIgD8Z8",
+            key: 9
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "Further",
+            author: "miini03",
+            mapInfo: {
+                simpleExp: "Far Flew Faster FURTHER",
+                uploadDate: "2020.04.26",
+                verifingAtt: "",
+                id: "696 / Free Copy",
+                long: "1분 04초",
+                highest: "11",
+                more: "",
+            },
+			percentToQualify: 65,
+            verificationVid: "https://www.youtube.com/watch?v=t2LCi-XjXv0",
+            key: 10
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "After On Track",
+            author: "Lebi",
+            mapInfo: {
+                simpleExp: "Back On Track 'After'",
+                uploadDate: "2020.05.13",
+                verifingAtt: "",
+                id: "2391 / Copy 불가",
+                long: "1분 29초",
+                highest: "12",
+                more: "",
+            },
+			percentToQualify: 56,
+            verificationVid: "https://www.youtube.com/watch?v=_M6-7ImVDYQ",
+            key: 11
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "AfterGeist",
+            author: "Lebi",
+            mapInfo: {
+                simpleExp: "Polargeist 'After'",
+                uploadDate: "2020.05.20",
+                verifingAtt: "",
+                id: "2924 / Copy 불가",
+                long: "1분 52초",
+                highest: "13",
+                more: "",
+            },
+			percentToQualify: 58,
+            verificationVid: "https://www.youtube.com/watch?v=krTYYwsBDp4",
+            key: 12
+        },
+        /*=================================================================================*/
+        {
+            vids: [
+            ],
+            name: "inner demons",
+            author: "cubic",
+            mapInfo: {
+                simpleExp: "My first XL level, Good luck! / 18k objects",
+                uploadDate: "2020.05.20",
+                verifingAtt: "",
+                id: "2940 / 006843",
+                long: "2분 22초",
+                highest: "14",
+                more: "",
+            },
+			percentToQualify: 60,
+            verificationVid: "https://www.youtube.com/watch?v=rz9SR6IIGG8",
+            key: 13
+        },
+        /*=================================================================================*/
+        /*
+        {
+            vids: [{
+                user: "WhiteDwarf",
+                link: "https://www.youtube.com/watch?v=ug24xGWYzFI",
+                percent: 60
+            }, ],
+            name: "God Eater",
+            author: "knobbelboy",
+            mapInfo: {
+                simpleExp: "",
+                uploadDate: "2019.01.06",
+                verifingAtt: "-",
+                long: "1분 27초",
+                highest: "36위",
+                more: "none",
+            },
+            percentToQualify: 56,
+            verificationVid: "https://www.youtube.com/watch?v=C9r5Upnq8eI",
+            legacyKey: 0
+        }*/
+        /*=================================================================================*/
+    ],
+    "version": [
+        1,
+        5,
+        1
+    ]
+};
+var list = d.list;
+const version = d.version;
---
+++ demonlist/js/userrank.js
@@ -0,0 +1,134 @@
+
+
+var userData = []; //{name : username, point : point, records : [{rank, percent, link, hz}], verified : [rank]}
+for (var i = 0; i < list.length; i++) {
+    var rank = i+1;
+    var entry = list[i];
+    var isVerifier = false;
+    for (var a = 0 ; a < entry.vids.length ; a++) {
+        var record = entry.vids[a];
+        var isLoot = false, islegit = true;
+        for (var r = 0; r < userData.length; r++) {
+            if (record.user.toUpperCase() == userData[r].name.toUpperCase()) {
+                isLoot = true;
+            }
+        }
+        for (var u = 0; u < ban_users.length ; u++) {
+          if (ban_users[u].user.toUpperCase() == record.user.toUpperCase()) {
+            islegit = false;
+            const itemToFind = list[i].vids.find(function(item) {return item.user.toUpperCase() === ban_users[u].user.toUpperCase()});
+            const idx = list[i].vids.indexOf(itemToFind);
+            if (idx > -1) list[i].vids.splice(idx, 1)
+          }
+        }
+        if (islegit) {
+          if (!isLoot) {
+            var userRecord = []; var verifyRecord = [];
+            userRecord.push({rank : rank, percent : record.percent, link: record.link, hz: record.hz==null ? "144hz" : record.hz});
+            userData.push({name: record.user, point: getPoint(rank, record.percent, entry.percentToQualify, record.hz), records : userRecord, verified : verifyRecord});
+          } else {
+            for (var b = 0 ; b < userData.length ; b++) {
+                if (userData[b].name.toUpperCase() == record.user.toUpperCase()) {
+                    var userRecord = userData[b].records;
+                    userRecord.push({rank : rank, percent : record.percent, link: record.link, hz: record.hz==null ? "144hz" : record.hz});
+                    userData[b].point = roundNumber(userData[b].point + getPoint(rank, record.percent, entry.percentToQualify, record.hz), 3);
+                    userData[b].records = userRecord;
+                }
+            }
+          }
+        } else {
+          a--;
+        }
+    }
+}
+
+for (var i = 0; i < list.length; i++) {
+  var verifier = "";
+  if (list[i].author.split("[").length != 2) {
+      verifier = list[i].author;
+  } else { verifier = list[i].author.split("[")[1].replace("]", ""); }
+  var isFirst = false;
+  for (var r = 0; r < userData.length; r++) {
+    if (verifier.toUpperCase() == userData[r].name.toUpperCase()) {
+      var userRecord = userData[r].verified;
+      userRecord.push(i+1);
+      userData[r].point = roundNumber(userData[r].point + getPoint(i+1, 100, list[i].percentToQualify, null), 3);
+      userData[r].verified = userRecord;
+      isFirst = true;
+    }
+  }
+  if (!isFirst) {
+    var userRecord = []; var verifyRecord = [];
+    verifyRecord.push(i+1)
+    userData.push({name: verifier, point: getPoint(i+1, 100, 99, "144hz"), records : userRecord, verified : verifyRecord});
+  }
+}
+
+userData.sort(function(a,b) {
+    return b.point - a.point;
+});
+
+function getPoint(rank, percent, cutline, hz) {
+  var hzpoint = 0.0;
+  if (hz == null) {
+      hzpoint = 1;
+  } else if (hz=="Mobile") {
+      hzpoint = 2.0;
+  } else {
+      hzInt = hz.replace(/[^0-9]/g,'');
+      if (hzInt >= 100) {
+          hzpoint = 1;
+      } else if (hzInt >= 70) {
+          hzpoint = 1.25;
+      } else {
+          hzpoint = 1.5;
+      }
+  }
+  if (rank > 50) {
+    return 0;
+  } else {
+      var score = roundNumber((((100/Math.sqrt(((rank-1)/30)+0.35431))-68)*hzpoint)*((percent - (cutline-1))/(100 - (cutline-1))), 3);
+      if (percent != 100) {
+        return roundNumber(score - (score/3), 3);
+      } else {
+        return score;
+      }
+  }
+}
+
+function roundNumber(num, scale) {
+    if(!("" + num).includes("e")) {
+      return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+    } else {
+      var arr = ("" + num).split("e");
+      var sig = ""
+      if(+arr[1] + scale > 0) {
+        sig = "+";
+      }
+    return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+    }
+}
+
+
+function getTrophy(rank) {
+    var score = rank / userData.length * 100;
+    var point = userData[rank-1].point;
+    if (rank == 1) {
+      return "top1_trophy.png";
+    } else if (point >= 500) {
+      return "gold_trophy.png";
+    } else if (point >= 400) {
+      return "silver_trophy.png";
+    } else if (point >= 250) {
+      return "cooper_trophy.png";
+    } else if (point >= 100) {
+      return "green_trophy.png";
+    } else if (point >= 50) {
+      return "aqua_trophy.png";
+    } else if (point >= 10) {
+      return "pink_trophy.png";
+    } else {
+      return "red_trophy.png";
+    }
+}
+
